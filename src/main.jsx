91e3ea7b36101444c4806b9ac07933c198ad05ae
Emma Adey <eadey@usf.edu>
2025-04-20 12:47:42 -0400

    Mount App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './style.css'
+import './style.css' // Import the main App component
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // Render the main component
   <StrictMode>
     <App />
   </StrictMode>,
